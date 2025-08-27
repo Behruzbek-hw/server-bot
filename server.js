9127d85035f7af4a8b6185b80d7eef4850b3e085
Behruzbek-hw <126411745+Behruzbek-hw@users.noreply.github.com>
2025-08-27 12:02:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const wss = new WebSocket.Server({ server });
